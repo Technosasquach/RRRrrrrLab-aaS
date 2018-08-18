9771bdf1e1e0bacc13c4af5bca640d1083e0c0aa
James Clare <jcjamescclare@gmail.com>
2018-08-18 16:42:39 +1000

    Built display component, added simple styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,33 @@
 import * as React from "react";
-
 import Axios from "axios";
 
-export default class App extends React.Component {
+import TextDisplay from "./textDisplay";
+
+import "./App.less"
+
+interface AppState {
+  response: String;
+}
+
+export default class App extends React.Component<{}, AppState> {
+
+  constructor(props: any){
+    super(props);
+
+    this.state = {
+      response: null
+    }
+    
+    this.handleClick = this.handleClick.bind(this);
+  }
 
   handleClick(){
     Axios.post("/api/")
       .then((resp) => {
-        console.log(resp)
+        console.log(resp);
+        this.setState({
+          response: resp.data.message
+        });
       })
       .catch((err) => {
         console.log(err)
@@ -16,13 +36,12 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="pageWrapper">
+        <div className="titleBar"> ☠️ Swashbuckle</div>
         <div onClick={this.handleClick}>
           Click me
         </div>
-        <div>
-          Your returned values is:
-        </div>
+        <TextDisplay placeholder={"Click the button to run a script"} content={this.state.response}/>
       </div>
     );
   }
