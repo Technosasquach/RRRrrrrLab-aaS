69b246e724a85993029ebf715c51aea847afb6a4
James Clare <jcjamescclare@gmail.com>
2018-08-18 14:45:04 +1000

    Added Axios to query api

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,23 @@
 import * as React from "react";
 
+import Axios from "axios";
+
 export default class App extends React.Component {
+
+  handleClick(){
+    Axios.post("/api/")
+      .then((resp) => {
+        console.log(resp)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
   render() {
     return (
       <div>
-        <div>
+        <div onClick={this.handleClick}>
           Click me
         </div>
         <div>
