ac5cba449f65c3dbf1bbe37be18c5b874a1bb279
matanui159 <josh@minteronline.com>
2018-08-18 14:18:58 +1000

    Simple string lexer

---
+++ CLI_/src/main/java/lexer/Lexer.java
@@ -0,0 +1,51 @@
+package lexer;
+
+import java.io.*;
+
+public class Lexer {
+    private Reader reader;
+    private int peeked = -1;
+
+    public Lexer(Reader reader) {
+        this.reader = reader;
+    }
+
+    private int peekChar() throws IOException {
+        if (peeked == -1) {
+            peeked = reader.read();
+        }
+        return peeked;
+    }
+
+    private int nextChar() throws IOException {
+        int result = peekChar();
+        peeked = -1;
+        return result;
+    }
+
+    private String nextString() throws IOException {
+        StringBuilder builder = new StringBuilder();
+        for (;;) {
+            int c = peekChar();
+            if (c == -1 || !Character.isAlphabetic(c)) {
+                return builder.toString();
+            }
+            builder.append((char)nextChar());
+        }
+    }
+
+    public Token nextToken() throws Exception {
+        while (Character.isWhitespace(peekChar())) {
+            nextChar();
+        }
+
+        int c = peekChar();
+        if (Character.isAlphabetic(peekChar())) {
+            return new Token(nextString());
+        } else if (c == -1) {
+            return new Token();
+        } else {
+            throw new Exception("Unknown character");
+        }
+    }
+}
--- CLI_/src/main/java/test/Test.java
+++ CLI_/src/main/java/test/Test.java
@@ -1,7 +1,14 @@
 package test;
 
+import lexer.*;
+import java.io.*;
+
 public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception {
+        Lexer lexer = new Lexer(new StringReader("Hello World  foo       bar"));
+        Token token;
+        while ((token = lexer.nextToken()).getType() != Token.Type.EOF) {
+            System.out.println(token.getString());
+        }
     }
 }
